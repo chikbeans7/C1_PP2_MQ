2d4991fe3be22581faa4d5255de2e14be2e9773a
chikbeans7 <adamcurrie50@gmail.com>
2022-06-21 06:29:03 +0000

    Add styles to the first page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,9 @@
-const txt1 = document.getElementById('uname')
-const btn1 = document.getElementById('cont')
-const out1 = document.getElementById('output1')
+const txt1 = document.getElementById('uname');
+const btn1 = document.getElementById('cont');
+const out1 = document.getElementById('output1');
 
 function getUserName(){
     out1.innerHTML = txt1.value;
 }
 
-btn1.addEventListener('click', getUserName)
+btn1.addEventListener('click', getUserName);
