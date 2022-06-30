15f21ef1b30727938c8fd4781c6e5fbda99a8943
chikbeans7 <adamcurrie50@gmail.com>
2022-06-30 09:59:33 +0000

    Add queryselectors to gather values from the groups of radio buttons to see which one has been checked

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,17 @@ const txt1 = document.getElementById('uname');
 const btn1 = document.getElementById('cont');
 const out1 = document.getElementById('output1');
 
+const q1 = document.querySelectorAll('input=[name="direcstar"]');
+const q2 = document.querySelectorAll('input=[name="jord"]');
+const q3 = document.querySelectorAll('input=[name="toy"]');
+const q4 = document.querySelectorAll('input=[name="rocky"]');
+const q5 = document.querySelectorAll('input=[name="djan"]');
+const q6 = document.querySelectorAll('input=[name="term"]');
+const q7 = document.querySelectorAll('input=[name="vilstar"]');
+const q8 = document.querySelectorAll('input=[name="pot1"]');
+const q9 = document.querySelectorAll('input=[name="pot2"]');
+const q10 = document.querySelectorAll('input=[name="iron"]');
+
 function getUserName(){
     out1.innerHTML = txt1.value;
 }
