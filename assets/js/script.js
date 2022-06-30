bbf7563fd0fd76322bbef04f2926c442d22b027b
chikbeans7 <adamcurrie50@gmail.com>
2022-06-30 10:12:04 +0000

    Start attempting to get the page to display a number one when the first question is ansered correctly

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,10 +12,24 @@ const q7 = document.querySelectorAll('input=[name="vilstar"]');
 const q8 = document.querySelectorAll('input=[name="pot1"]');
 const q9 = document.querySelectorAll('input=[name="pot2"]');
 const q10 = document.querySelectorAll('input=[name="iron"]');
+const totalScore = document.getElementById('score');
+const btn2 = document.getElementById('finish');
 
 function getUserName(){
     out1.innerHTML = txt1.value;
 }
 
-btn1.addEventListener('click', getUserName);
+function getScore(){
+    let answer;
+    let score = 0;
+    for(const radioButton of q1) {
+        if(radioButton.checked & radioButton.value == "correct"){
+            let score = score + 1;
+            break;
+        }
+    }
+    totalScore.innerHTML = score.value; 
+}
 
+btn1.addEventListener('click', getUserName);
+btn2.addEventListener('click', getScore);
