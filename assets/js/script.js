9380bc05315b6018391c809e7b4a22923d05a480
chikbeans7 <adamcurrie50@gmail.com>
2022-09-20 10:54:07 +0000

    Starting adding function that changes color of the option in the quiz the user is hovering over

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,6 +6,7 @@ const out1 = document.getElementById('output1');
 //Collects all the data from the question radio buttons
 const totalScore = document.getElementById('score');
 const btn2 = document.getElementById('finish');
+const questions = document.querySelectorAll('.question-area');
 
 let counter = 0;
 
@@ -13,9 +14,19 @@ let counter = 0;
 btn1.addEventListener('click', getUserName);
 btn2.addEventListener('click', testFun);
 
+for(let i = 0; i < questions.length; i++){
+    const hoverDiv = questions[i].querySelectorAll('.inline');
+    for(let i = 0; i < hoverDiv.length; i++){
+        hoverDiv.addEventListener('mouseover',changeColor);
+    }
+}
+
+function changeColor(event){
+    this.style.backgroundColor = "green";
+}
+
 function testFun(){
     let score = 0;
-    const questions = document.querySelectorAll('.question-area');
     for(let i = 0; i < questions.length; i++){
         const answers = questions[i].querySelectorAll('.inline');
         for(let i = 0; i < answers.length; i++){
@@ -35,33 +46,3 @@ function getUserName(){
     document.getElementById("displayuname").innerHTML = '<h2>Welcome '+uname.value+'!</h2>';
     uname.value = "";    
 }
-
-/* function getScore(){
-    
-    let score = 0;
-    //Loops through each questions array of radio buttons and sees if they are checked and if it is the correct answer
-    for(let i=0; i < questions.length; i++){
-        const current = questions[i];
-        const answers = current.querySelectorAll('.inline');
-        for(let i of answers){
-            let answer = answers.querySelectorAll
-            if(answer.checked){
-                if(button[1].value === 'correct'){
-                    score++;
-                    break;
-                }
-            }
-        }
-    } */
-
-   /*  // Displays the score and adds a well done image to the bottom of the screen
-    
-    let img = document.createElement("img");
-    img.src = "assets/images/welldone.png";
-
-    if(counter < 1){
-        let div = document.getElementById("img-container");
-        div.appendChild(img);
-    }
-    counter++;
-} */
